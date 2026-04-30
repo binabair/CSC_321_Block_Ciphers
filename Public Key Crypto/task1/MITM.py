e44d37e12cf53449dd54a005e5c177ed0ea75d72
david108 <datalave@gmail.com>
2026-04-30 14:26:10 -0700

    MITM added

--- Public Key Crypto/task1/MITM.py
+++ Public Key Crypto/task1/MITM.py
@@ -4,6 +4,7 @@ from Crypto.Random import get_random_bytes
 from Crypto.Protocol import DH
 from Crypto.Random import random
 
+
 class Person:
     q : int
     alpha : int
@@ -20,14 +21,34 @@ class Person:
         self.q = q
         self.alpha = a
         self.generateX()
+    def computes(self):
+        y = (self.alpha**self.x) % self.q
+        return y 
+    
+    def send_q(self, Person: Person, new_q: int ):
+        Person.q = new_q
 
 
-alice = Person()
-bob = Person()
-alice.q = 37
-alice.alpha = 5
-alice.send_q_a(bob)
 
+Alice = Person()
+Bob = Person()
+Mallory = Person()
+Alice.q = 37
+Alice.alpha = 5
+Alice.send_q_a(Bob)
+Alice.send_q_a(Mallory)
+
+Ya = Alice.computes()
+Yb = Bob.computes
+
+Mallory.send_q(Bob, Ya)
+Mallory.send_q(Alice, Yb)
+
+sa = Alice.computes()
+sb = Bob.computes()
+
+ka = f"SHA256{sa}"
+kb = f"SHA256{sb}"
 
 
 
