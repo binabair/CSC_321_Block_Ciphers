ea3966d5b008290637d8265b325288ea600ff66b
Tyler Pike <yosemitebone@gmail.com>
2026-05-12 15:53:19 -0700

    Finished up task 3

--- Public Key Crypto/task1/test.py
+++ Public Key Crypto/task1/test.py
@@ -54,7 +54,8 @@ class Person:
     def receive_y(self, y):
         self.yOther = y
         self.s = pow(self.yOther, self.x, self.q)
-        self.k = SHA256.new(bytes(self.s)).digest()
+        byte_length = (self.s.bit_length() + 7) // 8
+        self.k = SHA256.new(self.s.to_bytes(byte_length, byteorder='big')).digest()
         self.cipher = AES.new(self.k, AES.MODE_ECB)
 
 
@@ -92,8 +93,8 @@ class Person:
 
 alice = Person()
 bob = Person()
-alice.q = 37
-alice.alpha = 5
+alice.q = int.from_bytes(bytes.fromhex("B10B8F96A080E01DDE92DE5EAE5D54EC52C99FBCFB06A3C69A6A9DCA52D23B616073E28675A23D189838EF1E2EE652C013ECB4AEA906112324975C3CD49B83BFACCBDD7D90C4BD7098488E9C219A73724EFFD6FAE5644738FAA31A4FF55BCCC0A151AF5F0DC8B4BD45BF37DF365C1A65E68CFDA76D4DA708DF1FB2BC2E4A4371"))
+alice.alpha = int.from_bytes(bytes.fromhex("A4D1CBD5C3FD34126765A442EFB99905F8104DD258AC507FD6406CFF14266D31266FEA1E5C41564B777E690F5504F213160217B4B01B886A5E91547F9E2749F4D7FBD7D3B9A92EE1909D0D2263F80A76A6A24C087A091F531DBF0A0169B6A28AD662A4D18E73AFA32D779D5918D08BC8858F4DCEF97C2A24855E6EEB22B3B2E5"))
 alice.send_q_a(bob)
 
 alice.sendY(bob)
--- Public Key Crypto/task3/rsa.py
+++ Public Key Crypto/task3/rsa.py
@@ -6,6 +6,8 @@ import math
 import Crypto.Random
 import Crypto.Util as util
 
+
+
 def mod_inverse(a, n):
     t, newt = 0, 1
     r, newr = n, a
@@ -23,7 +25,7 @@ def mod_inverse(a, n):
 
     return t
 
-#Feel free to use your cryptographic library’s interface for generating large primes,
+# Feel free to use your cryptographic library’s interface for generating large primes,
 # but implement the rest-including computing the multiplicative inverse - yourself.
 
 e = 65537
@@ -45,47 +47,48 @@ def decrypt(encrpyted_message : int):
     length = math.ceil(message.bit_length() / 8)
     return message.to_bytes(length, 'big').decode()
 
-class Person:
-    n : int
-    e : int
-    d : int
-    s : int
-    k : int
+def sign(message : str):
+    message = int.from_bytes(message.encode(), byteorder='big')
+    signature = pow(message, d, n)
+    return signature
 
-    def __str__(self):
-        return ("q:" + str(self.q) + "\n"
-                + "alpha:" + str(self.alpha) + "\n"
-                + "x:" + str(self.x) + "\n"
-                + "y:" + str(self.y) + "\n"
-                + "other y:" + str(self.yOther) + "\n"
-                + "s:" + str(self.s)
-                + "k:" + str(self.k))
+def unsign(signature : int):
+    message = pow(signature, e, n)
 
+    return message
 
-    def generateY(self):
-        self.y = pow(self.alpha, self.x, self.q)
-
-    def send_n_e(self, bob : Person):
-        bob.receive_q_a(self.q, self.alpha)
-        self.generateX()
-        self.generateY()
-
-    def receive_n_e(self, n, e):
-        self.n = n
-        self.e = e
-        self.generateX()
-        self.generateY()
+def str_to_int(message : str):
+    return int.from_bytes(message.encode(), byteorder='big')
 
 
 
-alice = Person()
-bob = Person()
-alice.e = e
+alice_n = n
+alice_e = e
+bob_n = n
+bob_e = e
+bob_c = encrypt("This is bob and alice's key") # never makes it to alice
+mallory_c = encrypt("This is the key!!") # mallory can encrpyt since the info for this is public
+alice_k = decrypt(mallory_c)
+print("Alice's key: " + str(alice_k))
+mallory_k = "This is the key!!"
+print("Mallory key: " + str(mallory_k))
+if alice_k == mallory_k:
+    print("Mallory successfully has gotten the same key as Alice")
+else:
+    print("Mallory failed to have the same key as Alice")
 
 
 
-message = "hello world"
-encrypted_message = encrypt(message)
-print("Encrypted message: " + str(encrypted_message))
-decrypted_message = decrypt(encrypted_message)
-print("Decrypted message: " + decrypted_message)
+alice_signature_1 = sign("message 1")
+message_1 = str_to_int("message 1")
+alice_signature_2 = sign("message 2")
+message_2 = str_to_int("message 2")
+message_3 = (message_1 * message_2) % n
+signature_3 = (alice_signature_1 * alice_signature_2) % n
+if unsign(signature_3) == message_3:
+    print("Mallory successfully has created the signature for message 3")
+else:
+    print("Mallory failed to have created the signature for message 3")
+
+
+
